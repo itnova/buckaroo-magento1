213734f0b8e97b367b196c4142210ca07e5565c3
Kevin van Dijk <kevin.van.dijk@tig.nl>
2019-05-02 10:28:34 +0200

    added apple pay in checkout

--- app/code/community/TIG/Buckaroo3Extended/Block/Adminhtml/System/Config/Responsehint.php
+++ app/code/community/TIG/Buckaroo3Extended/Block/Adminhtml/System/Config/Responsehint.php
@@ -31,6 +31,7 @@ class TIG_Buckaroo3Extended_Block_Adminhtml_System_Config_Responsehint
         'afterpay',
         'afterpay2',
         'masterpass',
+        'applepay',
     );
 
     public $services = array(
