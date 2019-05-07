79f2f541bcc77dd0b11af700eb1e6aa5fd5b697b
Kevin van Dijk <kevin.van.dijk@tig.nl>
2019-05-07 13:38:59 +0200

    fixed old code

--- app/code/community/TIG/Buckaroo3Extended/Block/PaymentMethods/Applepay/ApplepayProduct.php
+++ app/code/community/TIG/Buckaroo3Extended/Block/PaymentMethods/Applepay/ApplepayProduct.php
@@ -8,34 +8,34 @@ class TIG_Buckaroo3Extended_Block_PaymentMethods_Applepay_ApplepayProduct
         parent::_construct();
     }
 
-//    public function getCurrency()
-//    {
-//        $currency = Mage::app()->getStore()->getCurrentCurrencyCode();
-//
-//        return $currency;
-//    }
-//
-//    public function getCountryCode()
-//    {
-//        $countryCode = substr(Mage::getStoreConfig('general/locale/code'), 3);
-//
-//        return $countryCode;
-//    }
-//
-//    public function getStoreName()
-//    {
-//        $storeName = Mage::app()->getStore()->getFrontendName();
-//
-//        return $storeName;
-//    }
-//
-//    public function getProductPrice()
-//    {
-//        $taxHelper  = $this->helper('tax');
-//        $product = $this->getProduct();
-//        $productPrice = $taxHelper->getPrice($product, $product->getFinalPrice());
-//
-//        return $productPrice;
-//    }
+    public function getCurrency()
+    {
+        $currency = Mage::app()->getStore()->getCurrentCurrencyCode();
+
+        return $currency;
+    }
+
+    public function getCountryCode()
+    {
+        $countryCode = substr(Mage::getStoreConfig('general/locale/code'), 3);
+
+        return $countryCode;
+    }
+
+    public function getStoreName()
+    {
+        $storeName = Mage::app()->getStore()->getFrontendName();
+
+        return $storeName;
+    }
+
+    public function getProductPrice()
+    {
+        $taxHelper  = $this->helper('tax');
+        $product = $this->getProduct();
+        $productPrice = $taxHelper->getPrice($product, $product->getFinalPrice());
+
+        return $productPrice;
+    }
 }
 
