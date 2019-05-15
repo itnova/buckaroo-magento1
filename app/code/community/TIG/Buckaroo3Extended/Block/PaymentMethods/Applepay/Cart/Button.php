996fbfd84babc1020895dd81e38bdbdbf81f999d
Kevin van Dijk <kevin.van.dijk@tig.nl>
2019-05-15 15:10:42 +0200

    BUCKM1-592: Added phpdocs.

--- app/code/community/TIG/Buckaroo3Extended/Block/PaymentMethods/Applepay/Cart/Button.php
+++ app/code/community/TIG/Buckaroo3Extended/Block/PaymentMethods/Applepay/Cart/Button.php
@@ -2,28 +2,46 @@
 class TIG_Buckaroo3Extended_Block_PaymentMethods_Applepay_Cart_Button
     extends Mage_Adminhtml_Block_Abstract
 {
+    /** @var bool $isProductPage */
     protected $isProductPage = false;
 
+    /**
+     * TIG_Buckaroo3Extended_Block_PaymentMethods_Applepay_Cart_Button constructor.
+     */
     public function __construct()
     {
         parent::_construct();
     }
 
+    /**
+     * @return string
+     * @throws Mage_Core_Model_Store_Exception
+     */
     public function getCurrency()
     {
         return Mage::app()->getStore()->getCurrentCurrencyCode();
     }
 
+    /**
+     * @return string
+     */
     public function getCountryCode()
     {
         return 'NL';
     }
 
+    /**
+     * @return string
+     * @throws Mage_Core_Model_Store_Exception
+     */
     public function getStoreName()
     {
         return Mage::app()->getStore()->getFrontendName();
     }
 
+    /**
+     * @return string
+     */
     public function getSubtotalText()
     {
         $config = Mage::getStoreConfig('tax/cart_display/subtotal');
@@ -35,16 +53,25 @@ class TIG_Buckaroo3Extended_Block_PaymentMethods_Applepay_Cart_Button
         return $this->__('Subtotal');
     }
 
+    /**
+     * @return float
+     */
     public function getSubtotal()
     {
         return round(Mage::getModel('checkout/cart')->getQuote()->getBaseGrandTotal(), 2);
     }
 
+    /**
+     * @return float
+     */
     public function getGrandTotal()
     {
         return round(Mage::getModel('checkout/cart')->getQuote()->getGrandTotal(),2);
     }
 
+    /**
+     * @return mixed
+     */
     public function getCultureCode()
     {
         $localeCode   = Mage::app()->getLocale()->getLocaleCode();
@@ -53,6 +80,9 @@ class TIG_Buckaroo3Extended_Block_PaymentMethods_Applepay_Cart_Button
         return $shortLocale;
     }
 
+    /**
+     * @return mixed
+     */
     public function getGuid()
     {
         $quote = Mage::getModel('checkout/cart')->getQuote();
@@ -72,6 +102,9 @@ class TIG_Buckaroo3Extended_Block_PaymentMethods_Applepay_Cart_Button
         $this->isProductPage = false;
     }
 
+    /**
+     * @return string
+     */
     public function getControllerUrl()
     {
         $this->setProductPage();
