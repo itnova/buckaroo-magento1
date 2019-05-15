9f636db78328448edc12a86ea7ef16cf123d53cf
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2019-05-15 10:58:22 +0200

    BUCKM1-592 - Added a way to get the product id

--- app/code/community/TIG/Buckaroo3Extended/Block/PaymentMethods/Applepay/Product/Button.php
+++ app/code/community/TIG/Buckaroo3Extended/Block/PaymentMethods/Applepay/Product/Button.php
@@ -1,39 +1,9 @@
 <?php
 class TIG_Buckaroo3Extended_Block_PaymentMethods_Applepay_Product_Button
-    extends Mage_Catalog_Block_Product_View
+    extends TIG_Buckaroo3Extended_Block_PaymentMethods_Applepay_Cart_Button
 {
-    public function __construct()
+    public function getProductId()
     {
-        parent::_construct();
-    }
-
-    public function getCurrency()
-    {
-        $currency = Mage::app()->getStore()->getCurrentCurrencyCode();
-
-        return $currency;
-    }
-
-    public function getCountryCode()
-    {
-        $countryCode = substr(Mage::getStoreConfig('general/locale/code'), 3);
-
-        return $countryCode;
-    }
-
-    public function getStoreName()
-    {
-        $storeName = Mage::app()->getStore()->getFrontendName();
-
-        return $storeName;
-    }
-
-    public function getProductPrice()
-    {
-        $taxHelper  = $this->helper('tax');
-        $product = $this->getProduct();
-        $productPrice = $taxHelper->getPrice($product, $product->getFinalPrice());
-
-        return $productPrice;
+        return Mage::registry('current_product');
     }
 }
