cf52a8d1aa92954fa2c30ee668d25ce57f3b4266
Kevin van Dijk <kevin.van.dijk@tig.nl>
2019-06-19 15:54:32 +0200

    BUCKM1-592: Added check for the aftersuccessaction so we only restore the cart when the paymentmethod is applepay

--- app/code/community/TIG/Buckaroo3Extended/Model/Observer/ApplepayAfterSuccess.php
+++ app/code/community/TIG/Buckaroo3Extended/Model/Observer/ApplepayAfterSuccess.php
@@ -3,9 +3,14 @@ class TIG_Buckaroo3Extended_Model_Observer_ApplepayAfterSuccess extends Mage_Cor
 {
     public function afterSuccessAction(Varien_Event_Observer $observer)
     {
-        /** @var TIG_Buckaroo3Extended_Model_PaymentMethods_Applepay_Process $process */
-        $process  = Mage::getModel(' buckaroo3extended/paymentMethods_applepay_process');
+        $orderId = $observer->getOrderIds()[0];
+        $paymentMethod = Mage::getModel('sales/order')->load($orderId)->getPayment()->getMethod();
         
-        $process->restoreCart();
+        if ($paymentMethod === 'buckaroo3extended_applepay') {
+            /** @var TIG_Buckaroo3Extended_Model_PaymentMethods_Applepay_Process $process */
+            $process = Mage::getModel(' buckaroo3extended/paymentMethods_applepay_process');
+            
+            $process->restoreCart();
+        }
     }
 }
